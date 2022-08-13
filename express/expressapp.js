d478a21442dc42861fbb71e079e3852fa2f787a8
ice-bear <andydowsen47us@gmail.com>
2022-08-13 14:43:27 +0530

    Express app commit

--- express/expressapp.js
+++ express/expressapp.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+const eventHandler = require("events");
+
+const eventManager = new eventHandler();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.statusCode = 200;
+  const data = fs.readFileSync("../html/index.html");
+  res.send(data.toString());
+  setTimeout((req, res) => {
+    eventManager.on("cache-generate", (err, data) => {
+      fs.writeFile(
+        `../backup/backup-log${+1}.txt`,
+        "document.cookie",
+        (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Cookie log generated in backup directory.");
+          }
+        }
+      );
+    });
+    eventManager.emit("cache-generate");
+  }, 1000);
 });
 
 app.listen(port, () => {
