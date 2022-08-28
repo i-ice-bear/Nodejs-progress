a35fbcbc76c133f6b4e9b85a13097fa7f4720634
ice-bear <andydowsen47us@gmail.com>
2022-08-28 13:19:34 +0530

    Async with real time data fetching

---
+++ JS/async/async.js
@@ -0,0 +1,16 @@
+function resolveData() {
+  return new Promise((resolve) => {
+    setTimeout(async () => {
+        const url = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2022-07-28&sortBy=publishedAt&apiKey=cd6f44f57521402dab39db73fde51eb4")
+        const jsonOutput = await url.json();
+        console.log(jsonOutput)
+    }, 2000);
+  });
+}
+
+async function CallData() {
+  console.log("Calling");
+  const awaits = await resolveData();
+  console.log(awaits);
+}
+CallData();
