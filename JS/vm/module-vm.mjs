6a42b8760c050c3092149de267c4fa1bfb3a4ba3
ice-bear <andydowsen47us@gmail.com>
2022-11-03 13:36:29 +0530

    Virtual-machine module usage with other modules

--- JS/vm/module-vm.mjs
+++ JS/vm/module-vm.mjs
@@ -1,1 +1,20 @@
-import express from "express"
+import express from "express";
+import vm from "node:vm";
+import process from "dotenv";
+
+const context = {
+  express: express,
+};
+
+vm.createContext(context);
+
+const code = `
+  const app = express()
+  const port = 3000
+  app.get('/', (req, res) => {
+    res.send('Hello, kitty!')
+  })
+  app.listen(port)
+  `;
+
+vm.runInContext(code, context);
